6387b9d509c5edf09a7e650f6650bcfe2d3fd698
fineigor6-debug <fineigor6@gmail.com>
2026-03-10 03:48:28 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+// TELEGRAM INIT
+const tg = window.Telegram.WebApp
+tg.ready()
+
+// USER DATA
+const user = tg.initDataUnsafe?.user
+
+if(user){
+
+const username=document.getElementById("username")
+const avatar=document.getElementById("avatar")
+
+if(username){
+username.innerText=user.first_name || "Player"
+}
+
+if(avatar && user.photo_url){
+avatar.src=user.photo_url
+}
+
+}
+
+// NAVIGATION
+
+function openCases(){
+window.location.href="cases.html"
+}
+
+function goBack(){
+window.history.back()
+}
