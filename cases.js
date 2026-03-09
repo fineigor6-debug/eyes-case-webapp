1ada8c829f41ee49b09ba1ef7db42389de49a104
fineigor6-debug <fineigor6@gmail.com>
2026-03-10 03:49:02 +0400

    Create cases.js

---
+++ cases.js
@@ -0,0 +1,13 @@
+function openDailyCase(){
+
+localStorage.setItem("caseType","daily")
+window.location.href="case.html"
+
+}
+
+function openTestCase(){
+
+localStorage.setItem("caseType","test")
+window.location.href="case.html"
+
+}
