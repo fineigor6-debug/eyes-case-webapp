604d3ced6a8838285b3ff9422ce6c05aa9282953
fineigor6-debug <fineigor6@gmail.com>
2026-03-09 20:17:15 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,47 @@ let tg = window.Telegram.WebApp
 
 tg.expand()
 
+/* ПОЛУЧАЕМ ПОЛЬЗОВАТЕЛЯ TELEGRAM */
+
 let user = tg.initDataUnsafe.user
 
 if(user){
 
 let name = user.username || user.first_name
 
-document.getElementById("username").innerText = name
+let usernameBlock = document.getElementById("username")
+let avatarBlock = document.getElementById("avatar")
 
-if(user.username){
+if(usernameBlock){
+usernameBlock.innerText = name
+}
 
-document.getElementById("avatar").src =
+if(user.username && avatarBlock){
+
+avatarBlock.src =
 "https://t.me/i/userpic/320/" + user.username + ".jpg"
 
 }
 
 }
+
+/* ОТКРЫТЬ ЭКРАН С КЕЙСАМИ */
+
+function openCases(){
+
+window.location.href = "cases.html"
+
+}
+
+/* НАЗАД */
+
+function goBack(){
+
+window.location.href = "index.html"
+
+}
+
+/* ОТКРЫТИЕ КЕЙСА */
 
 function openCase(){
 
