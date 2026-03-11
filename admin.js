f01f3323f132c9f10c82a51ab53aecfc312687c7
fineigor6-debug <fineigor6@gmail.com>
2026-03-11 20:53:58 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -23,3 +23,29 @@ alert("Баланс выдан")
 })
 
 }
+
+function adminRemoveStars(){
+
+let id = document.getElementById("adminUserId").value
+let stars = parseInt(document.getElementById("adminStars").value)
+
+fetch("http://localhost:3000/removeBalance",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+id:id,
+amount:stars
+})
+
+})
+.then(res=>res.json())
+.then(()=>{
+alert("Баланс снят")
+})
+
+}
