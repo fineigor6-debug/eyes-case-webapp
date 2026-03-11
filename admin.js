0c5550917d2fce955fe9c79547477e8e48b0d05c
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 01:07:43 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,20 @@
 const API = "https://bold-dew-c931.fineigor6.workers.dev"
-const ADMIN_KEY = "peel"
+const ADMIN_KEY = "superSecretAdminKey"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const giveBtn = document.querySelector(".btn-give")
+const removeBtn = document.querySelector(".btn-remove")
+
+if(giveBtn){
+giveBtn.addEventListener("click", adminGiveStars)
+}
+
+if(removeBtn){
+removeBtn.addEventListener("click", adminRemoveStars)
+}
+
+})
 
 function adminGiveStars(){
 
@@ -7,19 +22,15 @@ const id = document.getElementById("adminUserId").value
 const stars = parseInt(document.getElementById("adminStars").value)
 
 fetch(API + "/add-balance",{
-
 method:"POST",
-
 headers:{
 "Content-Type":"application/json"
 },
-
 body:JSON.stringify({
 id:id,
 amount:stars,
 key:ADMIN_KEY
 })
-
 })
 .then(res=>res.json())
 .then(data=>{
@@ -34,19 +45,15 @@ const id = document.getElementById("adminUserId").value
 const stars = parseInt(document.getElementById("adminStars").value)
 
 fetch(API + "/add-balance",{
-
 method:"POST",
-
 headers:{
 "Content-Type":"application/json"
 },
-
 body:JSON.stringify({
 id:id,
 amount:-stars,
 key:ADMIN_KEY
 })
-
 })
 .then(res=>res.json())
 .then(data=>{
