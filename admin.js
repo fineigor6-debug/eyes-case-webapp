f7fd155a7bd8a81bcd30c0331592a7798fe0ef93
fineigor6-debug <fineigor6@gmail.com>
2026-03-12 00:34:39 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,47 +1,5 @@
-// ----------------------
-// CONFIG
-// ----------------------
-
 const API = "https://bold-dew-c931.fineigor6.workers.dev"
-const ADMIN_ID = 8528585798
-const ADMIN_KEY = "pulvglazanavasnassal"
-
-// ----------------------
-// TELEGRAM USER
-// ----------------------
-
-let user = null
-
-if (window.Telegram && window.Telegram.WebApp) {
-
-const tg = window.Telegram.WebApp
-tg.ready()
-
-if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
-user = tg.initDataUnsafe.user
-}
-
-}
-
-// ----------------------
-// ADMIN CHECK
-// ----------------------
-
-if(!user || user.id !== ADMIN_ID){
-
-document.body.innerHTML = `
-<h2 style="text-align:center;margin-top:50px">
-Access denied
-</h2>
-`
-
-throw new Error("Not admin")
-
-}
-
-// ----------------------
-// GIVE BALANCE
-// ----------------------
+const ADMIN_KEY = "superSecretAdminKey"
 
 function adminGiveStars(){
 
@@ -63,24 +21,12 @@ key:ADMIN_KEY
 })
 
 })
-
 .then(res=>res.json())
-.then(data=>{
-
+.then(()=>{
 alert("Баланс выдан")
-
-})
-.catch(()=>{
-
-alert("Ошибка сервера")
-
 })
 
 }
-
-// ----------------------
-// REMOVE BALANCE
-// ----------------------
 
 function adminRemoveStars(){
 
@@ -102,17 +48,9 @@ key:ADMIN_KEY
 })
 
 })
-
 .then(res=>res.json())
-.then(data=>{
-
+.then(()=>{
 alert("Баланс снят")
-
-})
-.catch(()=>{
-
-alert("Ошибка сервера")
-
 })
 
 }
